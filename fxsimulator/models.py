ad155beaae6e09414d479c9e3a99a5df9b5735a7
kisxo <udaysubba2004@gmail.com>
2024-07-26 02:27:42 +0000

    updated stock model for proper representation

--- fxsimulator/models.py
+++ fxsimulator/models.py
@@ -7,3 +7,6 @@ def current_unix_time():
 class Stock(models.Model):
     period = models.PositiveBigIntegerField(default = current_unix_time)
     price = models.FloatField()
+
+    def __str__(self):
+        return f'{self.period}, {self.price}'
