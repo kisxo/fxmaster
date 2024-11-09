ed13ea2c07464415facd34acd0cf14fd4aabb44c
kisxo <udaysubba2004@gmail.com>
2024-11-09 23:05:21 +0530

    add django-rest-framework
    slowing transitioning backend to REST-ful

--- fxmaster/settings.py
+++ fxmaster/settings.py
@@ -47,9 +47,18 @@ INSTALLED_APPS = [
     'channels',
     'allauth',
     'allauth.account',
+    'rest_framework',
 ]
 
 AUTH_USER_MODEL = 'fxsimulator.User'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
