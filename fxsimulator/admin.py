bccf072fe585375d0d4279fbff14c82e24636728
kisxo <udaysubba2004@gmail.com>
2024-11-09 21:58:32 +0530

    add orders to admin panal

--- fxsimulator/admin.py
+++ fxsimulator/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Order
 
 class CustomUserAdmin(UserAdmin):
     model = User
     list_display = ['email', 'username', 'balance', 'first_name', 'last_name', 'is_staff']
 
 admin.site.register(User, CustomUserAdmin)
+admin.site.register(Order)
