e15b6138cd9e52643acb8123367dbe4c174563c7
kisxo <udaysubba2004@gmail.com>
2024-11-13 10:27:02 +0530

    update serializer to customize create method

--- fxsimulator/serializers.py
+++ fxsimulator/serializers.py
@@ -17,3 +17,18 @@ class OrderSerializer(serializers.ModelSerializer):
       write_fields = ['duration', 'side', 'amount']
       for field_name in write_fields:
         self.fields[field_name].read_only = False
+
+  def create(self, data):
+    current_stock_data = cache.get("current_stock")
+    
+    #TODO select current auth user 
+    data['user'] = User.objects.get(id = 1)
+    data['profit_margin'] = 80
+    
+    data['opening_id'] = current_stock_data.id
+    data['opening_period'] = current_stock_data.period
+    data['opening_price'] = current_stock_data.price
+    data['closing_id'] = current_stock_data.id + (12 * data['duration'])# 60 sec / 5 sec = 12 sec. Multiply every minute by 12
+    order = Order.objects.create(**data)
+    
+    return data
