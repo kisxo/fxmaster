40e347cfe0b8320bd716979a60595e3c7e527667
kisxo <udaysubba2004@gmail.com>
2024-11-13 10:25:59 +0530

    update serializer to default to readonly

--- fxsimulator/serializers.py
+++ fxsimulator/serializers.py
@@ -1,7 +1,19 @@
 from rest_framework import serializers
-from .models import Order
+from django.core.cache import cache
+from .models import Order, User
 
 class OrderSerializer(serializers.ModelSerializer):
   class Meta:
     model = Order
     fields = '__all__'
+    
+  # funtion set all fields read-only except some fields
+  def __init__(self, *args, **kwargs):
+      super().__init__(*args, **kwargs)
+      # Set all fields to read-only by default
+      for field_name, field in self.fields.items():
+          field.read_only = True
+      
+      write_fields = ['duration', 'side', 'amount']
+      for field_name in write_fields:
+        self.fields[field_name].read_only = False
