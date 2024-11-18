b73168aeb1c79ceb543763d01c78c1f35d137aaf
kisxo <udaysubba2004@gmail.com>
2024-11-18 22:33:39 +0530

    add better forex simulation

--- fxsimulator/tasks.py
+++ fxsimulator/tasks.py
@@ -10,6 +10,56 @@ from django.core.cache import cache
 channel_layer = get_channel_layer()
 room_id = "fxupdateroom"
 
+def normal_random(mu=0, sigma=1):
+    # Box-Muller transform to generate two independent standard normal variables
+    u1 = random.random()
+    u2 = random.random()
+    
+    z0 = math.sqrt(-2 * math.log(u1)) * math.cos(2 * math.pi * u2)
+    z1 = math.sqrt(-2 * math.log(u1)) * math.sin(2 * math.pi * u2)
+    
+    # Scale and shift to get the desired mean and standard deviation
+    return mu + sigma * z0
+
+
+@shared_task
+def forexSimulator():
+    # Parameters
+    S0 = 100  # Initial stock price
+    mu = 0.20  # Drift coefficient (annualized)
+    sigma = 0.25  # Volatility (annualized)
+    T = 1  # Time period in years (1 year simulation)
+    dt = 1 / (365 * 24 * 60)  # Time step in years for 1 minute (convert minutes to years)
+
+    # Initialize variables
+    try: 
+        price = cache.get("last_price")
+        if price is None:
+            price = S0
+    except:
+        price = S0  # Start with the initial price
+
+    # Simulate GBM for 1 step of 1 minute
+    dW = normal_random(0, math.sqrt(dt))  # Brownian motion increment
+    print(price)
+    price = price * math.exp((mu - 0.5 * sigma**2) * dt + sigma * dW)  # Update price
+
+    current_stock_price = price
+    current_period = int(time.time() * 1000)
+
+    cache.set("last_price", current_stock_price)
+
+    async_to_sync(channel_layer.group_send)(
+        room_id,
+        {
+            "type": "fxupdate",
+            "data": {
+                "period": current_period,
+                "price": current_stock_price,
+            },
+        }
+    )
+ 
 
 @shared_task
 def stockSimulator():
@@ -21,8 +71,8 @@ def stockSimulator():
         last_stock_price = 300
     
     #variables for simulating GBM
-    mu = 0.05  # Drift coefficientig 
-    sigma = 0.4  # Volatility
+    mu = 0.2  # Drift coefficientig 
+    sigma = 0.8  # Volatility
     dt = 0.050  # Time step
 
     # Generate a standard normal random variable using Box-Muller transform
