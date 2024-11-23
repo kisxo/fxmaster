8c16a70f8d2ad111ad18f24024a8bec639bc23a2
kisxo <udaysubba2004@gmail.com>
2024-11-23 15:12:58 +0530

    add initial stock data-loading from server
     stock data is fetched in the +page.js and from the +page.svelte file data in passed into FxChart component where it is finally rendered

--- frontend/src/routes/lobby/+page.svelte
+++ frontend/src/routes/lobby/+page.svelte
@@ -1,9 +1,13 @@
 <script>
   import FxChart from "./FxChart.svelte";
+
+  const {data} = $props();
+  console.log(data.stockData);
+  console.log("+page.js");
 </script>
 
 <h1> 
   Lobby
 </h1>
 
-<FxChart/>
+<FxChart stockData={data.stockData}/>
--- frontend/src/routes/lobby/FxChart.svelte
+++ frontend/src/routes/lobby/FxChart.svelte
@@ -2,58 +2,71 @@
   import Highcharts, { chart } from 'highcharts/highstock';
   import PriceIndicator from 'highcharts/modules/price-indicator';
   PriceIndicator(Highcharts);
-  import Indicators from 'highcharts/indicators/indicators-all';
-  Indicators(Highcharts);
   import Accessibility from 'highcharts/modules/accessibility';
   Accessibility(Highcharts);
   import { StockChart } from '@highcharts/svelte';
 
-  const options = {
+  let { stockData } = $props();
+  console.log(stockData);
+
+  let options = {
     title: {
         text: 'EURO / USD'
     },
     series: [{
-        type: 'line',
-    }]
-  };
-
-  options.series[0].data = [
-    [
-        1317888000000,
-        100.5101,
-    ],
-];
+      type: 'area',
+      data: stockData,
+      fillColor: {
+        linearGradient: {
+          x1: 0,
+          y1: 0,
+          x2: 0,
+          y2: 1
+        },
+        stops: [
+          [0, Highcharts.getOptions().colors[0]],
+          [
+            1,
+            Highcharts.color(
+                Highcharts.getOptions().colors[0]
+            ).setOpacity(0).get('rgba')
+        ]
+        ]
+      },
+      threshold: null,
+    }],
+  }
 
   const chatSocket = new WebSocket(
     'ws://localhost:8000/ws/fxupdatechannel/fxupdateroom/'
-);
+  );
 
-chatSocket.onmessage = function(e) {
-    const response = JSON.parse(e.data);
-    console.log(response);
+  chatSocket.onmessage = function(e) {
+      const response = JSON.parse(e.data);
+      console.log(response);
 
 
-    let newPoint = [
-        response.data.period,
-        response.data.price,
-    ]
+      let newPoint = [
+          response.data.period,
+          response.data.price,
+      ]
 
-};
+      CustomStockChart.series[0].addPoint(newPoint);
 
-chatSocket.onclose = function(e) {
-    console.error('Chat socket closed unexpectedly');
-};
+  };
 
-// Create the chart when the component mounts
-let CustomStockChart;
+  chatSocket.onclose = function(e) {
+      console.error('Chat socket closed unexpectedly');
+  };
 
-import { onMount } from 'svelte';
-onMount(() => {
-        CustomStockChart = Highcharts.stockChart('container', options);
-    }
-);
+  // Create the chart when the component mounts
+  let CustomStockChart;
+  $effect(() => {
+    CustomStockChart = Highcharts.stockChart('container', options);
+  });
 
 </script>
+
 
 <div id="container" style="height: 400px; min-width: 310px"></div>
 
