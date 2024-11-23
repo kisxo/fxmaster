2e74ad8c751af3d3f0b954dd90ef6860247981ba
kisxo <udaysubba2004@gmail.com>
2024-11-24 01:57:44 +0530

    update to axios for api calls

--- frontend/src/routes/lobby/+page.js
+++ frontend/src/routes/lobby/+page.js
@@ -1,7 +1,14 @@
+import axios from 'axios';
+
 export const load = async ({fetch, data}) => {
 
-    const res = await fetch("http://localhost:8000/fx/stocks/")
-    const extData = await res.json();
-    return {stockData: extData};
+    
+
+    let response = await axios.get("http://localhost:8000/fx/stocks/", {withCredentials: true})
+    if (response.status === 200)
+    {
+        return { stockData: response.data };
+    }
+
     
 }
