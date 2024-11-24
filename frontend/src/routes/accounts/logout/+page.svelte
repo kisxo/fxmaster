d8393f7bc6418faaf20f4d221cba84a8ae9c2c66
kisxo <udaysubba2004@gmail.com>
2024-11-24 20:41:00 +0530

    add logout feature

---
+++ frontend/src/routes/accounts/logout/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { goto } from "$app/navigation";
+    import Cookie from "js-cookie";
+    
+    async function logout() {
+        console.log("logout")
+
+        try {
+            let res = await fetch('http://localhost:8000/_allauth/browser/v1/auth/session', {
+                method: "DELETE",
+                credentials: "include",
+                headers: {
+                    "X-CSRFToken": Cookie.get("csrftoken"),
+                }
+            })
+
+            if(res.status === 401)
+            {
+                goto("/")
+            }
+        } catch (error) {
+            
+        }
+      
+    }
+</script>
+
+<button  on:click={logout}>Log out</button>
+
+<style>
+    button{
+        background-color: aqua;
+    }
+</style>
