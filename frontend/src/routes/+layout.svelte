929e63b88762f71a4bfc78d5b3ddf4fdfce61885
kisxo <udaysubba2004@gmail.com>
2024-11-24 13:03:10 +0530

    add csrf token to browser on initial  pageload

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,8 +1,22 @@
 <script>
+  import { onMount } from 'svelte';
 	import '../app.css';
 	let { children } = $props();
 
-  import Navbar from './Navbar.svelte';
+  	import Navbar from './Navbar.svelte';
+	import axios from 'axios';
+
+	onMount(() => {
+		axios.get('http://localhost:8000/csrf/',
+		{withCredentials: true})
+		.then(function (response) {
+			console.log(response);
+		})
+		.catch(function (error) {
+			console.log(error);
+		});
+	});
+    
 </script>
 
 <Navbar />
