04dc8df48661e2f326c136c0197bc94b55e607cb
kisxo <udaysubba2004@gmail.com>
2024-11-24 20:10:34 +0530

    add login requirement to lobby
    if current user is authenticated redirect to login page
    It checks if current user session is authenticated or not using backend api and redirects to login page if not

--- frontend/src/routes/lobby/+page.js
+++ frontend/src/routes/lobby/+page.js
@@ -1,14 +1,44 @@
+import Cookies from 'js-cookie';
 import axios from 'axios';
+import { goto, replaceState } from '$app/navigation';
+
+console.log("outside")
 
 export const load = async ({fetch, data}) => {
+    let stockData = [];
+    let isAllowed = false;
 
-    
+    try {
+        const res = await axios.get('http://localhost:8000/_allauth/browser/v1/auth/session', {withCredentials: true})
 
-    let response = await axios.get("http://localhost:8000/fx/stocks/", {withCredentials: true})
-    if (response.status === 200)
-    {
-        return { stockData: response.data };
+        console.log("lobby login check")
+        console.log(res)
+
+        if(res.status === 200)
+        {
+           isAllowed = true
+        }
+         
+    } catch (error) {
+        
     }
 
-    
+    try {
+        const res = await axios.get('http://localhost:8000/fx/stocks/', {withCredentials: true})
+
+        if(res.status === 200)
+        {
+            stockData = res.data;
+        }
+
+        
+    } catch (error) {
+        console.error("get stockdata error")
+    }
+
+    return {
+        isAllowed: isAllowed,
+        stockData: stockData
+    }
+
 }
--- frontend/src/routes/lobby/+page.svelte
+++ frontend/src/routes/lobby/+page.svelte
@@ -1,9 +1,14 @@
 <script>
+  import { goto } from "$app/navigation";
   import FxChart from "./FxChart.svelte";
 
   const {data} = $props();
   console.log(data.stockData);
   console.log("+page.js");
+  console.log(data)
+  if(!data.isAllowed){
+    goto("/accounts/login")
+  }
 </script>
 
 <h1> 
