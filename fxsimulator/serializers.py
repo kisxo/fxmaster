c59fa2f2565906c881f8566f15892411fbf65b03
kisxo <udaysubba2004@gmail.com>
2024-11-25 01:19:37 +0530

    add custom validation to serializer

--- fxsimulator/serializers.py
+++ fxsimulator/serializers.py
@@ -6,6 +6,20 @@ class OrderSerializer(serializers.ModelSerializer):
   class Meta:
     model = Order
     fields = '__all__'
+
+  def validate(self, data):
+    amount = data.get('amount')
+    # tempuser = User.objects.get(id = self.context['request'].user)
+    user = User.objects.get(id = self.context['request'].user.id)
+  
+    if amount > user.balance:
+       print("low balance['request]")
+       raise serializers.ValidationError('low balance')
+    else:
+       user.balance = user.balance - amount
+       user.save()
+
+    return data
     
   # funtion set all fields read-only except some fields
   def __init__(self, *args, **kwargs):
@@ -18,11 +32,19 @@ class OrderSerializer(serializers.ModelSerializer):
       for field_name in write_fields:
         self.fields[field_name].read_only = False
 
+  def validate_date(self):
+    # if self.start_date > self.end_date:
+    raise serializers.ValidationError("End date must be after start date.")
+
   def create(self, data):
+    user =  self.context['request'].user
     current_stock_data = cache.get("current_stock")
+    if current_stock_data is None:
+      current_stock_data = Stock.objects.all().last()
+
     
     #TODO select current auth user 
-    data['user'] = User.objects.get(id = 1)
+    data['user'] = User.objects.get(id = user.id)
     data['profit_margin'] = 80
     
     data['opening_id'] = current_stock_data.id
@@ -30,6 +52,7 @@ class OrderSerializer(serializers.ModelSerializer):
     data['opening_price'] = current_stock_data.price
     data['closing_id'] = current_stock_data.id + (12 * data['duration'])# 60 sec / 5 sec = 12 sec. Multiply every minute by 12
     order = Order.objects.create(**data)
+
     
     return data
   
