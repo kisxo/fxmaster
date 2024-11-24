da386926a5eebe13b9a5f848ee442bf8b2d80ffc
kisxo <udaysubba2004@gmail.com>
2024-11-24 13:35:57 +0530

    add X-CSRFToken header dynamically
    use js-cookie to get the csrf token from the cookie
    using axios interceptors add the csrf token dynamically

--- frontend/src/routes/+layout.js
+++ frontend/src/routes/+layout.js
@@ -1,2 +1,16 @@
 export const ssr = false;
 export const prerender = true;
+
+import Cookies from "js-cookie";
+import axios from "axios";
+
+axios.interceptors.request.use(
+    async function (config) {
+        config.headers["X-CSRFToken"] = Cookies.get('csrftoken');
+        console.log("intercept\n\n")
+        return config;
+    },
+    function (error) {
+      return Promise.reject(error);
+    }
+);
